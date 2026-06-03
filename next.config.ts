b5fd3d1e426325547600e317ca7157301e73f692
Kishore K <zishwrld@gmail.com>
2026-06-03 18:28:17 +0530

    e

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  distDir: 'build',
 };
 
 export default nextConfig;
-// Trigger Restart  
