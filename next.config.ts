b6608031e1aeeff2f7a086e06578b6e86fc60c8e
Kishore K <zishwrld@gmail.com>
2026-06-07 10:59:23 +0530

    Add sitemap rewrite to bypass GSC cache

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap-main.xml',
+        destination: '/sitemap.xml',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
