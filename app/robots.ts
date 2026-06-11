8aa285f633eab4ee159cd1c1bfd685e75a6f886e
Kishore K <zishwrld@gmail.com>
2026-06-11 16:15:08 +0530

    chore: enforce www canonical URLs

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://quascan.xyz/sitemap.xml',
+    sitemap: 'https://www.quascan.xyz/sitemap.xml',
   }
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://quascan.xyz'
+  const baseUrl = 'https://www.quascan.xyz'
 
   return [
     {
